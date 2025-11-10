69ae6ba2cb583340f7672d0db72d4827e2f74559
QINQIN <qinqin@MacBook-Pro-de-QINQIN.local>
2025-11-10 10:02:36 +0100

    Add Render domain to CORS_ALLOWED_ORIGINS

--- myproject/settings.py
+++ myproject/settings.py
@@ -125,10 +125,10 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5176",
-    "http://localhost:5177",
-    "http://localhost:5178",
     "http://localhost:5179",
     "http://localhost:5180",
     "http://localhost:5181",
+    "http://localhost:5182",
+    "http://localhost:5183",
+    "https://my-django-api-a5qf.onrender.com",
 ]
